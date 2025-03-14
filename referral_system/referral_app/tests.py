72cb0cf3dc8b44ee7b32646adad101d254fc96dc
elenaglu <shishalovae@mail.ru>
2025-03-14 20:09:30 +0500

    add ui-test for main api

--- referral_system/referral_app/tests.py
+++ referral_system/referral_app/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+import json
+import time
 
-# Create your tests here.
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+link = "http://127.0.0.1:8000/"
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+    button_number = browser.find_element(By.NAME, "phone_number")
+    button_number.send_keys("+79321132149")
+
+    button = browser.find_element(By.NAME, "send")
+    button.click()
+    time.sleep(1)
+    authentication = browser.find_element(By.TAG_NAME, "h3")
+    authentication_text = authentication.text
+    cookie = browser.get_cookies()
+    code = cookie[1]['value']
+    button_code = browser.find_element(By.NAME, "authentication_code")
+    button_code.send_keys(code)
+    button = browser.find_element(By.NAME, "send_ok")
+
+    assert "enter the code sent to your phone number" == authentication_text
+
+finally:
+    time.sleep(3)
+    browser.quit()
